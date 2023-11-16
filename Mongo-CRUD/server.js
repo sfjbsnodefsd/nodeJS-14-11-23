7df3cf5f40812756119bce56c9b8e0e2bf6eb334
ng.test88 <ng.test88@gmail.com>
2023-11-16 10:44:37 +0530

    added db connection

--- Mongo-CRUD/server.js
+++ Mongo-CRUD/server.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+app.get("/" , (req,res) =>{
+    res.send("Hello and welcome to the node js Rest API application")
+})
+
+
+mongoose.connect(process.env.DB_CONNECTION_URL).then(() => console.log('Connected!'));
+
+app.listen(process.env.PORT, () => {
+    console.log("App running on post 4000");
+})
+
