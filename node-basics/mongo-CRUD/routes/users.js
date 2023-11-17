e950a7b8e0c80249195cac315a8eed9d45d0d229
hiteshpatil12 <hiteshpatil394@gmail.com>
2023-11-17 04:35:46 +0000

    CRUD application completed

--- node-basics/mongo-CRUD/routes/users.js
+++ node-basics/mongo-CRUD/routes/users.js
@@ -25,16 +25,54 @@ router.get('/allusers', async(req, res) =>{
     }
 })
 
-router.delete('/delete/:userId', async(req, res) =>{
+
+router.get('/getUserById/:id', async(req, res) =>{
     try{
-        await User.remove({_id:req.params.userId});
-        res.status(200).join({
-            message:'User Deleted Successfully'
-        })
+        const {id} = req.params
+        const user = await User.findById(id)
+        if(!user){
+            return res.status(404).json({message:`Can not find any user with the id &{id}`})
+        }
+        res.status(200).json(user)
     }
     catch(err){
         res.json(err)
     }
 })
+
+router.delete('/delete/:id', async(req, res) =>{
+    try{
+        const {id} = req.params
+        const user = await User.findByIdAndDelete(id)
+        if(!user){
+            return res.status(404).json({message:`Can not find any user with the id &{id}`})
+        }
+        res.status(200).json(user)
+        // await User.remove({_id:req.params.userId});
+        // res.status(200).join({
+        //     message:'User Deleted Successfully'
+        // })
+    }
+    catch(err){
+        res.json(err)
+    }
+})
+
+router.put('/updateuser/:id', async(req, res)=>{
+    try{
+        const {id} =req.params;
+        const user = await User.findByIdAndUpdate(id,req.body)
+        if(!user){
+            return res.status(404).json({message:`Can not find any user with the id &{id}`})
+        }
+        const updatedUser =await User.findById(id)
+        res.status(200).json(updatedUser)
+    }
+    catch(error){
+        console.log();
+        res.status(500).json({message:err.message})
+    }
+})
+
 
 module.exports = router;
