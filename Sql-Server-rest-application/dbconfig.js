36dd795364bbf714ae928937fe4627509f234f20
ng.test88 <ng.test88@gmail.com>
2023-11-17 12:27:17 +0530

    created mssql server rest crud node js api application and did the db config, added all dependencies

---
+++ Sql-Server-rest-application/dbconfig.js
@@ -0,0 +1,14 @@
+const config = {
+  user: "root",
+  password: "root123",
+  server: "127.0.0.1",
+  database: "Products",
+  options: {
+    trustedconnection: true,
+    enableArithAort: true,
+    instanceName: "SQLEXPRESS",
+  },
+  port: 55892,
+};
+
+module.exports = config;
