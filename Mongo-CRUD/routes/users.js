5df9713b477a10f628b3777e713bb4d46abec075
ng.test88 <ng.test88@gmail.com>
2023-11-17 09:49:03 +0530

    added find by id

--- Mongo-CRUD/routes/users.js
+++ Mongo-CRUD/routes/users.js
@@ -43,4 +43,19 @@ router.delete("/delete/:id", async (req, res) => {
   }
 });
 
+// write a get method with pathe /finduser/id and use the method findById(id)
+
+router.get("/finduser/:id", async(req,res) => {
+  try{
+    const {id} = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({message:err.message})
+  }
+})
+
+
+
 module.exports = router;
